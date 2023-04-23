d658ecf0b84b4eb091a4cfcbb4ed4da23ab98def
Francis Carlo Manlangit <francismanlangit13@gmail.com>
2023-04-24 02:55:32 +0800

    fix staff

--- staff/home/code.php
+++ staff/home/code.php
@@ -1100,88 +1100,84 @@
     $mname= $_POST['mname'];
     $lname= $_POST['lname'];
     $email= $_POST['email'];
-    if(isset($_POST['barangay'])) {
-      $new_password= $_POST['password'];
-      $password = md5($new_password);
-    } else {
-      $sql = "SELECT `password` FROM `user` WHERE user_id = $user_id";
-      $sql_run = $con->query($sql);
-      $data = $sql_run->fetch_assoc();
-      $password = $data['password'];
+    if(isset($_POST['confirm_password']) && !empty($_POST['confirm_password'])){
+        $new_password= $_POST['password'];
+        $password = md5($new_password);
+        $sql0 = "UPDATE `user` SET `password` = '$password' WHERE `user_id` = '$user_id'";
+        $sql0_run = mysqli_query($con, $sql0);
     }
     if(isset($_FILES['image']) && is_uploaded_file($_FILES['image']['tmp_name']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
-      $fileImage = $_FILES['image'];
-      $OLDfileImage = $_POST['oldimage'];
-      $customFileName = 'user_' . date('Ymd_His'); // replace with your desired file name
-      $ext = pathinfo($fileImage['name'], PATHINFO_EXTENSION); // get the file extension
-      $fileName = $customFileName . '.' . $ext; // append the extension to the custom file name
-      $fileTmpname = $fileImage['tmp_name'];
-      $fileSize = $fileImage['size'];
-      $fileError = $fileImage['error'];
-      $fileExt = explode('.',$fileName);
-      $fileActExt = strtolower(end($fileExt));
-      $allowed = array('jpg','jpeg','png');
-      if(in_array($fileActExt, $allowed)){
+        $fileImage = $_FILES['image'];
+        $OLDfileImage = $_POST['oldimage'];
+        $customFileName = 'user_' . date('Ymd_His'); // replace with your desired file name
+        $ext = pathinfo($fileImage['name'], PATHINFO_EXTENSION); // get the file extension
+        $fileName = $customFileName . '.' . $ext; // append the extension to the custom file name
+        $fileTmpname = $fileImage['tmp_name'];
+        $fileSize = $fileImage['size'];
+        $fileError = $fileImage['error'];
+        $fileExt = explode('.',$fileName);
+        $fileActExt = strtolower(end($fileExt));
+        $allowed = array('jpg','jpeg','png');
+        if(in_array($fileActExt, $allowed)){
         if($fileError === 0){
-          if($fileSize < 10485760){
+            if($fileSize < 10485760){
             $uploadDir = '../../assets/img/users/';
             $targetFile = $uploadDir . $fileName;
             unlink($uploadDir . $OLDfileImage);
 
             if (move_uploaded_file($fileTmpname, $targetFile)) {
-              $query = "UPDATE `user` SET `fname`='$fname',`mname`='$mname',`lname`='$lname',`email`='$email',`password`='$password',`picture`='$fileName' WHERE `user_id`='$user_id'";
-              $query_run = mysqli_query($con, $query);
+                $query = "UPDATE `user` SET `fname`='$fname',`mname`='$mname',`lname`='$lname',`email`='$email',`picture`='$fileName' WHERE `user_id`='$user_id'";
+                $query_run = mysqli_query($con, $query);
     
-              if($query_run){
-                $_SESSION['status'] = "Account Updated";
+                if($query_run){
+                $_SESSION['status'] = "Account updated sucessfully";
                 $_SESSION['status_code'] = "success";
                 header('Location: index');
                 header("Location: " . base_url . "staff/home/");
                 exit(0);
-              }
-              else{
+                }
+                else{
+                $_SESSION['status'] = "Something went wrong!";
                 $_SESSION['status_code'] = "error";
                 header("Location: " . base_url . "staff/home/");
                 exit(0);
-              }
+                }
             }
-          }
-          else{
+            }
+            else{
             $_SESSION['status']="File is too large file must be 10mb";
             $_SESSION['status_code'] = "error"; 
             header("Location: " . base_url . "staff/home/settings");
-          }
+            }
         }
         else{
-          $_SESSION['status']="File Error";
-          $_SESSION['status_code'] = "error"; 
-          header("Location: " . base_url . "staff/home/settings");
+            $_SESSION['status']="File Error";
+            $_SESSION['status_code'] = "error"; 
+            header("Location: " . base_url . "staff/home/settings");
         }
-      }
-      else{
+        }
+        else{
         $_SESSION['status']="Invalid file type";
         $_SESSION['status_code'] = "error"; 
         header("Location: " . base_url . "staff/home/settings");
-      }
+        }
     }
     else{
-      $query = "UPDATE `product` SET `product_name`='$name',`product_quantity`='$quantity',`product_category_id`='$category',`product_status`='$status' WHERE `product_id`='$user_id'";
-      $query_run = mysqli_query($con, $query);
+        $query = "UPDATE `user` SET `fname`='$fname',`mname`='$mname',`lname`='$lname',`email`='$email' WHERE `user_id`='$user_id'";
+        $query_run = mysqli_query($con, $query);
 
-      $query = "UPDATE `user` SET `fname`='$fname',`mname`='$mname',`lname`='$lname',`email`='$email',`password`='$password' WHERE `user_id`='$user_id'";
-      $query_run = mysqli_query($con, $query);
-
-      if($query_run){
-        $_SESSION['status'] = "Account Updated";
+        if($query_run){
+        $_SESSION['status'] = "Account updated sucessfully";
         $_SESSION['status_code'] = "success";
         header("Location: " . base_url . "staff/home/");
         exit(0);
-      }
-      else{
+        }
+        else{
+        $_SESSION['status'] = "Something went wrong!";
         $_SESSION['status_code'] = "error";
-        header("Location: " . base_url . "staff/home/");
+        header("Location: " . base_url . "staff/home/settings");
         exit(0);
-      }
+        }
     }
   }
 
