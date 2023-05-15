abd4acb85410f9a557d2bc73b18f172d086e1d1c
Francis Carlo Manlangit <francismanlangit13@gmail.com>
2023-05-16 01:38:20 +0800

    fix homepage added contact forms

---
+++ forms/contact.php
@@ -0,0 +1,75 @@
+<?php
+  include ('../db_conn.php');
+  //use PHPMailer\PHPMailer\PHPMailer;
+  require("../assets/PHPMailer/PHPMailerAutoload.php");
+  require ("../assets/PHPMailer/class.phpmailer.php");
+  require ("../assets/PHPMailer/class.smtp.php");
+  $errors = [];
+  $errorMessage = '';
+
+  if (!empty($_POST)) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    if (empty($name)) {
+      $errors[] = 'Name is empty';
+    }
+
+    if (empty($email)) {
+      $errors[] = 'Email is empty';
+    }
+    else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $errors[] = 'Email is invalid';
+    }
+
+    if (empty($message)) {
+      $errors[] = 'Message is empty';
+    }
+
+    if (!empty($errors)) {
+      $allErrors = join('<br/>', $errors);
+      $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
+    }
+    else {
+      $mail = new PHPMailer();
+      
+      // specify SMTP credentials
+      $mail->isSMTP();
+      $mail->Host = 'smtp.gmail.com';
+      $mail->SMTPAuth = true;
+      $mail->Username = emailuser;
+      $mail->Password = emailpass;
+      $mail->SMTPSecure = 'TLS/STARTTLS';
+      $mail->Port = '587';
+      $mail->setFrom($email, 'Contact Municipal Agriculture Office');
+      $mail->addAddress(emailuser, 'Me');
+      $mail->Subject = 'New message from your website';
+
+      // Enable HTML if needed
+      $mail->isHTML(true);
+      $bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Message:", nl2br($message)];
+      $body = join('<br />', $bodyParagraphs);
+      $mail->Body = $body;
+      echo $body;
+
+      if($mail->send()) {
+        // Email sent successfully
+        // echo 'OK';
+      } else {
+        // Error sending email
+        // echo 'error';
+      }
+      // if($mail->send()){
+      //   header('Location: thank-you.html'); // Redirect to 'thank you' page. Make sure you have it
+      //   $_SESSION['status'] = "Thank You for emailing from us";
+      //   $_SESSION['status_code'] = "success";
+      //   header("Location: " . base_url);
+      //   exit(0);
+      // } 
+      // else {
+      //   $errorMessage = 'Oops, something went wrong. Mailer Error: ' . $mail->ErrorInfo;
+      // }
+    }
+  }
+?>
--- index.php
+++ index.php
@@ -90,7 +90,7 @@
     <!-- Main Selection -->
       <main id="main">
         <!-- ======= What We Do Section ======= -->
-          <section id="what-we-do" class="what-we-do">
+          <section id="about" class="what-we-do">
             <div class="container">
 
               <div class="section-title">
@@ -130,7 +130,7 @@
         <!-- End What We Do Section -->
 
         <!-- ======= About Section ======= -->
-          <section id="about" class="about">
+          <section id="what-we-do" class="about">
             <div class="container">
 
               <div class="row">
@@ -325,23 +325,30 @@
               </div>
 
             </div>
-
-            <div class="row mt-5 justify-content-center" hidden="true">
-              <div class="col-lg-10">
+            <div class="row mt-5 justify-content-center">
+              <!-- to edit google map goto https://www.embed-map.com type your location, generate html code and copy the html  -->
+              <div class="col-lg-6" style="max-width:100%;overflow:hidden;color:red;width:500px;height:500px;">
+                <div id="embedmap-canvas" style="height:100%; width:100%;max-width:100%;">
+                  <iframe style="height:100%;width:100%;border:0;" frameborder="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3947.711007559862!2d123.82512624785154!3d8.331487083362013!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32550301520a09c7%3A0x97790db3e2a40a38!2sJimenez%20Municipal%20Nursery!5e0!3m2!1sen!2sph!4v1684171147442!5m2!1sen!2sph"></iframe>
+                </div>
+                <a class="googlemaps-html" href="https://www.embed-map.com" id="get-data-forembedmap">https://www.embed-map.com</a>
+                <style>#embedmap-canvas img{max-width:none!important;background:none!important;font-size: inherit;font-weight:inherit;}</style>
+              </div>
+              <div class="col-lg-6">
                 <form action="forms/contact.php" method="post" role="form" class="php-email-form">
                   <div class="row">
                     <div class="col-md-6 form-group">
                       <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
                     </div>
                     <div class="col-md-6 form-group mt-3 mt-md-0">
-                      <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+                      <input type="email" class="form-control" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" id="email" placeholder="Your Email" required>
                     </div>
                   </div>
                   <div class="form-group mt-3">
                     <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
                   </div>
                   <div class="form-group mt-3">
-                    <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+                    <textarea class="form-control" name="message" rows="7" placeholder="Message" required></textarea>
                   </div>
                   <div class="my-3">
                     <div class="loading">Loading</div>
