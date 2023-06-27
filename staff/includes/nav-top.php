8c282c5ffb99a03be4bda30b6dba2b949f3f4d22
Francis Carlo Manlangit <francismanlangit13@gmail.com>
2023-06-27 10:52:47 +0800

    fix on print issue on scroll

--- staff/includes/nav-top.php
+++ staff/includes/nav-top.php
@@ -1,6 +1,6 @@
  
  <!-- Content Wrapper -->
-  <div id="content-wrapper" class="d-flex flex-column">
+  <div id="content-wrapper" class="d-flex flex-column noprint-scroll">
 
 <!-- Main Content -->
 <div id="content">
