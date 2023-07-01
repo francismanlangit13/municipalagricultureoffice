1028c3b32e6919a45af024e0f3ff5a0fc3f407b3
Francis Carlo Manlangit <francismanlangit13@gmail.com>
2023-07-01 09:18:25 +0800

    added privacy policy

---
+++ privacy-policy.php
@@ -0,0 +1,209 @@
+<?php
+  include ('db_conn.php');
+  include ('initialize.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Google Analytics -->
+    <script>
+      window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+      ga('create', 'G-LCVFDYEBW0', 'auto');
+      ga('send', 'pageview');
+    </script>
+    <script async src='<?php echo base_url ?>assets/js/analytics.js'></script>
+    <!-- End Google Analytics -->
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="A web-based Farmers monitoring management system" name="description">
+    <meta content="Monitoring, Management, System, Notification" name="keywords">
+    <meta name="google-site-verification" content="RUBWqhjH6EGoC1nXSwk_iFB6FV5WSjZ_jEn5zj3tcHU" />
+
+    <!--Title Page-->
+    <title>Privacy Policy | Municipal Agriculture Office Jimenez</title>
+
+    <!-- Remove Banner -->
+    <script src="<?php echo base_url ?>assets/js/fwhabannerfix.js"></script>
+    
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="<?php echo base_url ?>assets/img/system/favicon.png" type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo base_url ?>assets/img/system/favicon.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo base_url ?>assets/img/system/favicon.png">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="<?php echo base_url ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url ?>assets/css/custom.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="<?php echo base_url ?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="<?php echo base_url ?>assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="<?php echo base_url ?>assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="<?php echo base_url ?>assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Loading CSS -->
+    <link href="<?php echo base_url ?>assets/css/loader.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="<?php echo base_url ?>assets/css/style-cookie.css" rel="stylesheet">
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-LCVFDYEBW0"></script>
+    <script src="<?php echo base_url ?>assets/js/gtag.js"></script>
+
+    <!-- Google Tag Manager -->
+    <script src="<?php echo base_url ?>assets/js/gtagmanager.js"></script>
+
+    <!-- =======================================================
+    * Template Name: Lumia - v4.10.0
+    * Template URL: https://bootstrapmade.com/lumia-bootstrap-business-template/
+    * Author: BootstrapMade.com
+    * License: https://bootstrapmade.com/license/
+    ======================================================== -->
+  </head>
+
+  <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5D46BH6" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <!-- Loading Screen -->
+    <div id="loading">
+        <img id="loading-image" src="<?php echo base_url ?>assets/img/system/loading.gif" alt="Loading" />
+    </div>
+    <div id="connectionAlert" class="alert"></div>
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top d-flex align-items-center bg-success">
+      <div class="container d-flex align-items-center">
+
+        <div class="logo me-auto">
+          <h1>
+            <a href="<?php echo base_url ?>"><img src="<?php echo base_url ?>assets/img/system/favicon.png" alt="MAO Jimenez" aria-label="Municipal Agriculture Office" class="img-fluid"></a>
+            <a href="<?php echo base_url ?>" aria-label="Municipal Agriculture Office" data-alttext="MAO Jimenez"><b>Municipal Agriculture Office Jimenez</b></a>
+          </h1>
+          <!-- Uncomment below if you prefer to use an image logo -->
+          <!--== <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+        </div>
+        <nav id="navbar" class="navbar order-last order-lg-0">
+          <ul>
+            <li><a class="nav-link scrollto active" href="<?php echo base_url?>#hero">Home</a></li>
+            <li><a class="nav-link scrollto" href="<?php echo base_url?>#about">About</a></li>
+            <li><a class="nav-link scrollto " href="<?php echo base_url?>#portfolio">Pictures</a></li>
+            <li><a class="nav-link scrollto" href="<?php echo base_url?>#contact">Contact</a></li>
+            <li><a class="nav-link scrollto" href="<?php echo base_url?>#aboutus">About Us</a></li>
+            <li><a class="nav-link scrollto" href="<?php echo base_url?>#team">Team</a></li>
+            <li><a class="nav-link " href="<?php echo base_url ?>login">LOGIN</a></li>
+          </ul>
+          <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav><!-- .navbar -->
+
+
+
+      </div>
+    </header><!-- End Header -->
+
+    <!-- ======= Home Section ======= -->
+      <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+        <div class="container text-center text-md-left" data-aos="fade-up">
+          <h1>Privacy Policy</h1>
+          <h2>Is your information safe with Municipal Agriculture Office Jimenez?</h2>
+        </div>
+      </section>
+    <!-- End Home Selection -->
+
+    <!-- Main Selection -->
+      <main id="main">
+        <!-- ======= What We Do Section ======= -->
+          <section class="what-we-do">
+            <div class="container">
+
+              <div class="section-title">
+                <h4>Privacy Policy for maojimenez.ueuo.com</h4>
+                <p>This Privacy Policy governs the manner in which www.maojimenez.ueuo.com collects, uses, maintains, and discloses information collected from users (each, a "User") of the website www.maojimenez.ueuo.com ("Website"). This Privacy Policy applies to the Website and all products and services offered by www.maojimenez.ueuo.com.</p>
+              </div>
+              <label><b>1. Personal Identification Information</b></label>
+              <p style="text-align:justify">www.maojimenez.ueuo.com may collect personal identification information from Users in various ways, including, but not limited to, when Users visit the Website, register on the Website, subscribe to the newsletter, fill out a form, and in connection with other activities, services, features, or resources made available on the Website. Users may be asked for, as appropriate, name, email address, mailing address, and phone number. Users may, however, visit the Website anonymously. www.maojimenez.ueuo.com will collect personal identification information from Users only if they voluntarily submit such information. Users can always refuse to supply personal identification information, except that it may prevent them from engaging in certain Website-related activities.</p>
+              <label><b>2. Non-Personal Identification Information</b></label>
+              <p style="text-align:justify">www.maojimenez.ueuo.com may collect non-personal identification information about Users whenever they interact with the Website. Non-personal identification information may include the browser name, the type of computer, and technical information about Users' means of connection to the Website, such as the operating system and the Internet service providers utilized and other similar information.</p>
+              <label><b>3. Web Browser Cookies</b></label>
+              <p style="text-align:justify">The Website may use "cookies" to enhance the User experience. The User's web browser places cookies on their hard drive for record-keeping purposes and sometimes to track information about them. Users may choose to set their web browser to refuse cookies or to alert them when cookies are being sent. If they do so, note that some parts of the Website may not function properly.</p>
+              <label><b>4. How We Use Collected Information</b></label>
+              <p style="text-align:justify">www.maojimenez.ueuo.com may collect and use Users' personal information for the following purposes:<br>
+                <b>&#x2022; To improve customer service:</b> Information provided by Users helps us respond to customer service requests and support needs more efficiently.<br>
+                <b>&#x2022; To personalize the User experience:</b> We may use information in the aggregate to understand how our Users as a group use the services and resources provided on our website.<br>
+                <b>&#x2022; To send periodic emails:</b> We may use the email address to send User information and updates pertaining to their order or request. It may also be used to respond to their inquiries, questions, and/or other requests.
+              </p>
+              <label><b>5. How We Protect Your Information</b></label>
+              <p style="text-align:justify">We adopt appropriate data collection, storage, and processing practices and security measures to protect against unauthorized access, alteration, disclosure, or destruction of your personal information, username, password, transaction information, and data stored on our website.</p>
+              <label><b>6. Sharing Your Personal Information</b></label>
+              <p style="text-align:justify">We do not sell, trade, or rent Users' personal identification information to others. We may share generic aggregated demographic information not linked to any personal identification information regarding visitors and users with our business partners, trusted affiliates, and advertisers for the purposes outlined above.</p>
+              <label><b>7. Compliance with Data Privacy Act of the Philippines</b></label>
+              <p style="text-align:justify">www.maojimenez.ueuo.com complies with the Republic Act of the Philippines, specifically the Data Privacy Act. We ensure that personal information is collected, used, processed, disclosed, retained, and disposed of in a manner consistent with the provisions of the said act.</p>
+              <label><b>8. Changes to This Privacy Policy</b></label>
+              <p style="text-align:justify">www.maojimenez.ueuo.com has the discretion to update this Privacy Policy at any time. When we do, we will revise the updated date at the bottom of this page. We encourage Users to frequently check this page for any changes to stay.</p>
+            </div>
+          </section>
+        <!-- End What We Do Section -->
+
+      </main>
+    <!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+      <footer id="footer">
+
+        <div class="footer-top">
+          <div class="container">
+            <div class="row">
+
+            
+            </div>
+          </div>
+        </div>
+
+        <div class="container d-md-flex py-4">
+
+          <div class="me-md-auto text-center text-md-start">
+            <div class="copyright">
+              Copyright &copy; 1996-<?php echo date('Y'); ?> <strong><span>Municipal Agriculture Office Jimenez</span></strong>. All Rights Reserved
+            </div>
+            <a href="cookie-policy">Cookie Policy</a>
+            <a href="privacy-policy">Privacy Policy</a>
+            <a href="website-terms-condition">Terms of Use</a>
+          </div>
+          <div class="social-links text-center text-md-right pt-3 pt-md-0">
+            <a href="https://www.facebook.com/municipalagriculture.jimenez" aria-label="Visit Municipal Agriculture Office on Facebook" target="_blank"><i class="bx bxl-facebook"></i></a>
+          </div>
+        </div>
+      </footer>
+    <!-- End Footer -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="<?php echo base_url ?>assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="<?php echo base_url ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo base_url ?>assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="<?php echo base_url ?>assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="<?php echo base_url ?>assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="<?php echo base_url ?>assets/vendor/waypoints/noframework.waypoints.js"></script>
+    <script src="<?php echo base_url ?>assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Loading JS -->
+    <script src="<?php echo base_url ?>assets/js/loader.js"></script>
+
+    <!-- Serverstatus JS -->
+    <script src="<?php echo base_url ?>assets/js/serverstatus.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="<?php echo base_url ?>assets/js/main.js"></script>
+
+    <script>
+      var base_url = "<?php echo base_url ?>"; // Global base_url in javascript
+    </script>
+
+  </body>
+
+</html>
