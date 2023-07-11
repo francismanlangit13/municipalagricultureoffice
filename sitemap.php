e03b735e207c2d16e56856ce85f9781df08c4b2c
Francis Carlo Manlangit <francismanlangit13@gmail.com>
2023-07-12 02:36:43 +0800

    add sitemap.php

---
+++ sitemap.php
@@ -0,0 +1,344 @@
+<?php
+
+  include ('db_conn.php');
+
+  include ('initialize.php');
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+
+        <!-- Google Analytics -->
+
+        <script>
+
+        window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+
+        ga('create', 'G-LCVFDYEBW0', 'auto');
+
+        ga('send', 'pageview');
+
+        </script>
+
+        <script async src='<?php echo base_url ?>assets/js/analytics.js'></script>
+
+        <!-- End Google Analytics -->
+
+        <meta charset="utf-8">
+
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+        <meta content="A web-based Farmers monitoring management system" name="description">
+
+        <meta content="Monitoring, Management, System, Notification" name="keywords">
+
+        <meta name="google-site-verification" content="RUBWqhjH6EGoC1nXSwk_iFB6FV5WSjZ_jEn5zj3tcHU" />
+
+
+
+        <!--Title Page-->
+
+        <title>Site Map | Municipal Agriculture Office Jimenez</title>
+
+
+
+        <!-- Remove Banner -->
+
+        <script src="<?php echo base_url ?>assets/js/fwhabannerfix.js"></script>
+
+        
+
+        <!-- Favicons -->
+
+        <link rel="shortcut icon" href="<?php echo base_url ?>assets/img/system/favicon.png" type="image/x-icon">
+
+        <link rel="apple-touch-icon" sizes="120x120" href="<?php echo base_url ?>assets/img/system/favicon.png">
+
+        <link rel="apple-touch-icon" sizes="152x152" href="<?php echo base_url ?>assets/img/system/favicon.png">
+
+
+
+        <!-- Google Fonts -->
+
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+
+
+        <!-- Vendor CSS Files -->
+
+        <link href="<?php echo base_url ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+        <link href="<?php echo base_url ?>assets/css/custom.css" rel="stylesheet">
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <link href="<?php echo base_url ?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+
+        <link href="<?php echo base_url ?>assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+
+        <link href="<?php echo base_url ?>assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+
+        <link href="<?php echo base_url ?>assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+
+
+        <!-- Loading CSS -->
+
+        <link href="<?php echo base_url ?>assets/css/loader.css" rel="stylesheet">
+
+
+
+        <!-- Cookie CSS -->
+
+        <link href="<?php echo base_url ?>assets/css/cookie.css" rel="stylesheet">
+
+
+
+        <!-- Template Main CSS File -->
+
+        <link href="<?php echo base_url ?>assets/css/style-cookie.css" rel="stylesheet">
+
+
+
+        <!-- Google tag (gtag.js) -->
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-LCVFDYEBW0"></script>
+
+        <script src="<?php echo base_url ?>assets/js/gtag.js"></script>
+
+
+
+        <!-- Google Tag Manager -->
+
+        <script src="<?php echo base_url ?>assets/js/gtagmanager.js"></script>
+
+
+
+        <!-- =======================================================
+
+        * Template Name: Lumia - v4.10.0
+
+        * Template URL: https://bootstrapmade.com/lumia-bootstrap-business-template/
+
+        * Author: BootstrapMade.com
+
+        * License: https://bootstrapmade.com/license/
+
+        ======================================================== -->
+
+    </head>
+
+    <body>
+
+        <!-- Google Tag Manager (noscript) -->
+        <noscript>
+
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5D46BH6" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+
+        </noscript>
+        <!-- End Google Tag Manager (noscript) -->
+
+        <!-- Loading Screen -->
+        <div id="loading">
+            <img id="loading-image" src="<?php echo base_url ?>assets/img/system/loading.gif" alt="Loading" />
+        </div>
+
+        <div id="connectionAlert" class="alert"></div>
+
+        <!-- ======= Header ======= -->
+        <header id="header" class="fixed-top d-flex align-items-center bg-success">
+
+        <div class="container d-flex align-items-center">
+
+            <div class="logo me-auto">
+
+                <h1>
+                    <a href="<?php echo base_url ?>"><img src="<?php echo base_url ?>assets/img/system/favicon.png" alt="MAO Jimenez" aria-label="Municipal Agriculture Office" class="img-fluid"></a>
+                    <a href="<?php echo base_url ?>" aria-label="Municipal Agriculture Office" data-alttext="MAO Jimenez"><b>Municipal Agriculture Office Jimenez</b></a>
+                </h1>
+
+            <!-- Uncomment below if you prefer to use an image logo -->
+
+            <!--== <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+            </div>
+
+            <nav id="navbar" class="navbar order-last order-lg-0">
+
+                <ul>
+
+                    <li><a class="nav-link" href="<?php echo base_url?>#hero">Home</a></li>
+
+                    <li><a class="nav-link" href="<?php echo base_url?>#about">About</a></li>
+
+                    <li><a class="nav-link" href="<?php echo base_url?>#portfolio">Pictures</a></li>
+
+                    <li><a class="nav-link" href="<?php echo base_url?>#contact">Contact</a></li>
+
+                    <li><a class="nav-link" href="<?php echo base_url?>#aboutus">About Us</a></li>
+
+                    <li><a class="nav-link" href="<?php echo base_url?>#team">Team</a></li>
+
+                    <li><a class="nav-link" href="<?php echo base_url ?>login">LOGIN</a></li>
+
+                </ul>
+
+                <i class="bi bi-list mobile-nav-toggle"></i>
+
+            </nav><!-- .navbar -->
+
+        </div>
+
+        </header>
+        <!-- End Header -->
+
+        <!-- Main Selection -->
+
+        <main id="main">
+
+            <!-- ======= What We Do Section ======= -->
+            <section class="what-we-do">
+
+                <div class="container">
+
+                <br><br><br>
+
+                <div class="section-title">
+
+                    <h3>Sitemap</h3>
+
+                </div>
+
+                <label><b>&#11044; <a href="<?php echo base_url ?>#hero">Home</a></b></label>
+                    <p style="margin-left:30px;">&#9675; <a href="<?php echo base_url ?>#about">About</a></p>
+                    <p style="margin-left:30px;">&#9675; <a href="<?php echo base_url ?>#portfolio">Pictures</a></p>
+                    <p style="margin-left:30px;">&#9675; <a href="<?php echo base_url ?>#contact">Contact</a></p>
+                    <p style="margin-left:30px;">&#9675; <a href="<?php echo base_url ?>#aboutus">About Us</a></p>
+                    <p style="margin-left:30px;">&#9675; <a href="<?php echo base_url ?>#team">Team</a></p>
+                <label><b>&#11044; <a href="<?php echo base_url ?>login">Login</a></b></label>
+                    <p style="margin-left:30px;">&#9675; <a href="<?php echo base_url ?>login/qr">Login with QR Code</a></p>
+                    <p style="margin-left:30px;">&#9675; <a href="<?php echo base_url ?>login/forgot">Forgot password</a></p>
+                <label><b>&#11044; The Municipal Agriculture Office</b></label>
+                    <p style="margin-left:30px;">&#9675; <a href="<?php echo base_url ?>cookie-policy">Cookie Policy</a></p>
+                    <p style="margin-left:30px;">&#9675; <a href="<?php echo base_url ?>privacy-policy">Privacy Policy</a></p>
+                    <p style="margin-left:30px;">&#9675; <a href="<?php echo base_url ?>sitemap">Site Map</a></p>
+                    <p style="margin-left:30px;">&#9675; <a href="<?php echo base_url ?>website-terms-condition">Terms of Use</a></p>
+
+                </div>
+
+            </section>
+            <!-- End What We Do Section -->
+
+        </main>
+
+        <!-- End #main -->
+
+        <!-- Cookie Consent -->
+        <div class="wrapper">
+
+        <img src="<?php echo base_url ?>assets/img/icons/cookie.png" alt="">
+
+        <div class="content">
+
+            <header>Cookies Consent</header>
+
+            <p>Cookies help us deliver our services. By using our services, you agree to our use of cookies. <a href="<?php echo base_url ?>cookie-policy">Cookie Policy</a></p>
+
+            <div class="buttons">
+
+            <button class="item">I understand</button>
+
+            </div>
+
+        </div>
+
+        </div>
+        <!-- End Cookie Consent -->
+
+        <!-- ======= Footer ======= -->
+
+        <footer id="footer">
+
+            <div class="footer-top">
+
+            <div class="container">
+
+                <div class="row">
+
+                </div>
+
+            </div>
+
+            </div>
+
+            <div class="container d-md-flex py-4">
+
+            <div class="me-md-auto text-center text-md-start">
+
+                <div class="copyright">
+
+                Copyright &copy; 1996-<?php echo date('Y'); ?> <strong><span>Municipal Agriculture Office Jimenez</span></strong>. All Rights Reserved
+
+                </div>
+
+                <a href="cookie-policy">Cookie Policy</a>
+
+                <a href="privacy-policy">Privacy Policy</a>
+
+                <a href="website-terms-condition">Terms of Use</a>
+
+                <a href="sitemap">Sitemap</a>
+
+            </div>
+
+            <div class="social-links text-center text-md-right pt-3 pt-md-0">
+
+                <a href="https://www.facebook.com/municipalagriculture.jimenez" aria-label="Visit Municipal Agriculture Office on Facebook" target="_blank"><i class="bx bxl-facebook"></i></a>
+
+            </div>
+
+            </div>
+
+        </footer>
+
+        <!-- End Footer -->
+
+        <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+        <!-- Cookie Consent -->
+        <script src="<?php echo base_url ?>assets/js/cookie.js"></script>
+
+        <!-- Disable-key -->
+        <script src="<?php echo base_url ?>assets/js/disable-key.js"></script>
+
+        <!-- Vendor JS Files -->
+        <script src="<?php echo base_url ?>assets/vendor/purecounter/purecounter_vanilla.js"></script>
+        <script src="<?php echo base_url ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="<?php echo base_url ?>assets/vendor/glightbox/js/glightbox.min.js"></script>
+        <script src="<?php echo base_url ?>assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+        <script src="<?php echo base_url ?>assets/vendor/swiper/swiper-bundle.min.js"></script>
+        <script src="<?php echo base_url ?>assets/vendor/waypoints/noframework.waypoints.js"></script>
+        <script src="<?php echo base_url ?>assets/vendor/php-email-form/validate.js"></script>
+
+        <!-- Loading JS -->
+        <script src="<?php echo base_url ?>assets/js/loader.js"></script>
+
+        <!-- Serverstatus JS -->
+        <script src="<?php echo base_url ?>assets/js/serverstatus.js"></script>
+
+        <!-- Template Main JS File -->
+        <script src="<?php echo base_url ?>assets/js/main.js"></script>
+
+        <script>
+
+        var base_url = "<?php echo base_url ?>"; // Global base_url in javascript
+
+        </script>
+
+    </body>
+
+</html>
