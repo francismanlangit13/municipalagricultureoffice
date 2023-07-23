47b0f626c42860359e56f019c7d903d789e97eb6
Francis Carlo Manlangit <francismanlangit13@gmail.com>
2023-07-23 13:02:25 +0800

    add search bar

--- farmer/home/product.php
+++ farmer/home/product.php
@@ -1,13 +1,55 @@
 <?php
     include('../includes/header.php');
 ?>
+<style>
+    .search {
+        width: 100%;
+        position: relative;
+        display: flex;
+        justify-content: center;
+    }
+
+    .searchTerm {
+        width: 30%;
+        border: 3px solid #00B4CC;
+        border-right: none;
+        padding: 15.5px;
+        height: 36px;
+        border-radius: 5px 0 0 5px;
+        outline: none;
+        color: #9DBFAF;
+    }
+
+    .searchTerm:focus{
+        color: #00B4CC;
+    }
+
+    .searchButton {
+        width: 40px;
+        height: 36px;
+        border: 1px solid #00B4CC;
+        background: #00B4CC;
+        text-align: center;
+        color: #fff;
+        border-radius: 0 5px 5px 0;
+        cursor: pointer;
+        font-size: 20px;
+    }
+</style>
 
 <ol class="breadcrumb mb-4">    
   <li class="breadcrumb-item">Dashboard</li>
   <li class="breadcrumb-item">View Product</li>
 </ol>
 <h2 class="text-center mb-3">PRODUCT</h2>
-
+<div class="wrap mb-3">
+    <div class="search">
+        <input type="text" class="searchTerm" id="Search" onkeyup="myFunction()" placeholder="What are you looking for?" title="Type a product">
+        <button type="submit" class="searchButton">
+            <i class="fa fa-search"></i>
+        </button>
+    </div>
+</div>
 <div class="row"> 
     <div class="col-lg-12 col-md-12 col-sm-12">
         <div class="card-group">
@@ -26,7 +68,7 @@
                 if($product){
                     while($row = mysqli_fetch_assoc($query_run)){
             ?>
-            <div class="col-12 col-md-6 col-lg-3 mb-4">
+            <div class="col-12 col-md-6 col-lg-3 mb-4 target">
                 <a href="product_view?id=<?=$row['product_id'];?>" style="text-decoration:none; color:black;">
                     <div class="card h-100">
                         <img class="img-fluid card-img-top" src="<?php echo base_url ?>assets/img/products/<?php echo $row['photo'];?>"  alt="user-avatar" style= "height:250px; width: 100%; object-fit: cover;">
@@ -58,3 +100,18 @@
 
 <?php include('../includes/footer.php');?>
 
+<script>
+    function myFunction() {
+        var input = document.getElementById("Search");
+        var filter = input.value.toLowerCase();
+        var nodes = document.getElementsByClassName('target');
+
+        for (i = 0; i < nodes.length; i++) {
+            if (nodes[i].innerText.toLowerCase().includes(filter)) {
+                nodes[i].style.display = "block";
+            } else {
+                nodes[i].style.display = "none";
+            }
+        }
+    }
+</script>
